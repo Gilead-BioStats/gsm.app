636c63990f6bd5619801252cd9ce5e77abaa635d
Jeremy Wildfire <jwildfire@gmail.com>
2023-05-03 11:45:53 -0400

    add safetyGraphics charts

--- R/run_gsm_app.R
+++ R/run_gsm_app.R
@@ -17,7 +17,9 @@ run_gsm_app <- function(
     meta = gsmApp::meta_data_frame,#map_meta_to_safetyGraphics(),
     workflows = NULL,
     domain_data = NULL,
-    filter_domain = 'dfSUBJ'
+    filter_domain = 'dfSUBJ',
+    mapping=NULL,
+    includeSG=TRUE
 ) {
     stopifnot(
         '[ meta ] must be a data frame.' = is.data.frame(meta)
@@ -41,6 +43,70 @@ run_gsm_app <- function(
             })
     }
 
+    #Add safetyGraphics charts/meta/data
+    if(includeSG){
+        #Add safetyGraphics domains
+        domain_data$aes <- domain_data$dfAE
+        domain_data$labs <- domain_data$dfLB
+        domain_data$dm <- domain_data$dfSUBJ
+
+        #add charts
+        sgcharts <- safetyGraphics::makeChartConfig()
+        workflows <- c(workflows, sgcharts)
+
+        #add metadata 
+        sgmeta <- safetyGraphics::makeMeta(sgcharts)
+        meta <- bind_rows(
+            meta %>% select(-key, -value),
+            sgmeta %>% mutate(standard_gsm = "")
+        )
+
+        #update domains
+        domains <- unique(meta$domain)
+
+        #add default mapping
+        sgmapping <- list(
+            dm = list(
+                id_col = "subjectid", 
+                treatment_col = "", 
+                treatment_values = list(
+                    group1 = "", 
+                    group2 = ""
+                ), 
+                sex_col = "sex", 
+                race_col = "race", 
+                age_col = "agerep"
+            ), 
+            aes = list(
+                id_col = "subjectid", 
+                seq_col = "recordid", 
+                stdy_col = "", 
+                endy_col = "", 
+                term_col = "mdrpt_nsv", 
+                bodsys_col = "mdrsoc_nsv", 
+                severity_col = "aesev_std_nsv", 
+                serious_col = "aeser"
+            ), 
+            labs = list(
+                measure_col = "lbtstnam", 
+                measure_values = list(
+                    ALT = "ALT (SGPT)", 
+                    AST = "AST (SGOT)", 
+                    TB = "Total Bilirubin", 
+                    ALP = "Alkaline Phosphatase"
+                ), 
+                id_col = "subjid", 
+                value_col = "siresn", 
+                normal_col_low = "sinrlo", 
+                normal_col_high = "sinrhi", 
+                studyday_col = "", 
+                visit_col = "visnam", 
+                visitn_col = "visnum", 
+                unit_col = ""
+            )
+        )
+        mapping <- c(mapping,sgmapping)
+    }
     #overview_table <- prepare_overview_table(workflows, domains)
 
     # Launch app.
@@ -49,6 +115,7 @@ run_gsm_app <- function(
         domainData = domain_data,
         #charts = c(list(overview_table), workflows),
         charts = workflows,
+        mapping = mapping,
         filterDomain = filter_domain,
         appName = '{gsm} Explorer',
         hexPath = system.file('resources/gsm.png', package = 'gsmApp'),
