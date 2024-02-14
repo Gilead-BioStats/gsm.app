c52db7e5f60526e64555698fbf319c655fd4965f
Matt Roumaya <matt.roumaya@atorusresearch.com>
2024-02-14 15:37:49 +0000

    remove dropdown and dropdown label

--- R/mod_metric_details_server.R
+++ R/mod_metric_details_server.R
@@ -4,46 +4,31 @@
 
 metric_details_server <- function(id, snapshot, metric, site) {
     shiny::moduleServer(id, function(input, output, session) {
-        # output$scatter_plot <- render_widget_scatter_plot({
-        #     shiny::req(metric())
-        #
-        #     if (metric() == 'None')
-        #         return(NULL)
-        #
-        #     gsm_output <- snapshot$lStudyAssessResults[[ metric() ]]
-        #     data <- gsm_output$lResults$lData$dfSummary
-        #     config <- snapshot$lInputs$lMeta$meta_workflow %>%
-        #         dplyr::filter(
-        #             .data$workflowid == metric()
-        #         )
-        #     bounds <- gsm_output$lResults$lData$dfBounds
-        #
-        #     widget_scatter_plot(
-        #         data,
-        #         config,
-        #         bounds,
-        #         site()
-        #     )
-        # })
+
+
 
         output$scatter_plot <- gsm::renderWidget_ScatterPlot({
             shiny::req(metric())
             snapshot$lCharts[[ metric() ]]$scatterJS$x$selectedGroupIDs <- site()
-            # snapshot$lCharts[[ metric() ]]$scatterJS$x$addSiteSelect <- FALSE
+            snapshot$lCharts[[ metric() ]]$scatterJS$x$bIsShiny <- TRUE
+            snapshot$lCharts[[ metric() ]]$scatterJS$x$siteSelectLabelValue <- ""
+            snapshot$lCharts[[ metric() ]]$scatterJS
             snapshot$lCharts[[ metric() ]]$scatterJS
         })
 
         output$bar_chart_score <- gsm::renderWidget_BarChart({
             shiny::req(metric())
             snapshot$lCharts[[ metric() ]]$barScoreJS$x$selectedGroupIDs <- site()
-            # snapshot$lCharts[[ metric() ]]$barScoreJS$x$addSiteSelect <- FALSE
+            snapshot$lCharts[[ metric() ]]$barScoreJS$x$bIsShiny <- TRUE
+            snapshot$lCharts[[ metric() ]]$barScoreJS$x$siteSelectLabelValue <- ""
             snapshot$lCharts[[ metric() ]]$barScoreJS
         })
 
         output$bar_chart_metric <- gsm::renderWidget_BarChart({
             shiny::req(metric())
             snapshot$lCharts[[ metric() ]]$barMetricJS$x$selectedGroupIDs <- site()
-            # snapshot$lCharts[[ metric() ]]$barMetricJS$x$addSiteSelect <- FALSE
+            snapshot$lCharts[[ metric() ]]$barMetricJS$x$bIsShiny <- TRUE
+            snapshot$lCharts[[ metric() ]]$barMetricJS$x$siteSelectLabelValue <- ""
             snapshot$lCharts[[ metric() ]]$barMetricJS
         })
 
