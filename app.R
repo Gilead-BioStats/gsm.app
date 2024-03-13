b1a2dc361433eff52abfd67bb586ae998afa2fb9
Matt Roumaya <matt.roumaya@atorusresearch.com>
2024-03-13 21:08:19 +0000

    fix deployment process

--- app.R
+++ app.R
@@ -1,26 +1,4 @@
-# library(bslib)
-# library(dplyr)
-# library(gsm)
-# library(purrr)
-# library(shiny)
-# library(shinyjs)
-# library(stringr)
-# library(tidyr)
-# library(DT)
-# library(htmltools)
+pkgload::load_all(".")
 
-purrr::walk(
-    list.files('R', full.names = TRUE),
-    source
-)
-
-snapshot <- gsmApp::read_snapshot()
-
-    shinyApp(
-        ui = ui(),
-        server = function(input, output, session) {
-            server(input, output, session, snapshot)
-        }
-    )
-
+gsmApp::run_app()
 
