8d819fcfc552b0081b366a8facc9d09a0ecc1fa7
Matt Roumaya <matt.roumaya@atorusresearch.com>
2024-03-13 19:12:26 +0000

    modify app.R

--- app.R
+++ app.R
@@ -1,23 +1,20 @@
-library(bslib)
-library(dplyr)
-library(gsm)
-library(purrr)
-library(shiny)
-library(shinyjs)
-library(stringr)
-library(tidyr)
-library(DT)
-library(htmltools)
+# library(bslib)
+# library(dplyr)
+# library(gsm)
+# library(purrr)
+# library(shiny)
+# library(shinyjs)
+# library(stringr)
+# library(tidyr)
+# library(DT)
+# library(htmltools)
 
 purrr::walk(
     list.files('R', full.names = TRUE),
     source
 )
 
-#snapshot <- readRDS('data/snapshot.rds')
-load('data/snapshot.rda')
-
-#run_app(snapshot) # shiny::shinyApp()
+snapshot <- gsmApp::read_snapshot()
 
     shinyApp(
         ui = ui(),
