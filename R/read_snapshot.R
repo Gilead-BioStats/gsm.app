9076a130f9384533c16b3310b7d1903a93d34b93
Matt Roumaya <matt.roumaya@atorusresearch.com>
2024-03-15 17:00:37 +0000

    fix data issue and pass checks

--- R/read_snapshot.R
+++ R/read_snapshot.R
@@ -8,17 +8,10 @@
 #' }
 #' @export
 read_snapshot <- function() {
-    scattered_snapshots <- list.files(system.file("sampledata", package = "gsmApp"), full.names = TRUE)
 
-    all <- purrr::map(scattered_snapshots, function(x){
-        qs::qread(x)
-    }) %>%
-        purrr::set_names(sub("snapshot_", "", tools::file_path_sans_ext(basename(scattered_snapshots))))
+    sampledata <- qs::qread(system.file("sampledata", "snapshot.qs", package = "gsmApp"))
 
-
-    all$lSnapshotDate <- all$lSnapshot$lSnapshotDate
-    all$lSnapshot <- all$lSnapshot$lSnapshot
-    return(all)
+    return(sampledata)
 }
 
 
