4f1f4915849cc3a356e635a48dc621b556d4bad1
Jon Harmon <jon.harmon@atorusresearch.com>
2024-08-06 12:34:14 -0500

    Remove unused parameters.

--- R/aaa-shared.R
+++ R/aaa-shared.R
@@ -3,8 +3,6 @@
 #' Reused parameter definitions are gathered here for easier usage.
 #'
 #' @param id The namespace id for this module.
-#' @param combinedData `data.frame` A combined data set with the required
-#'   metrics.
 #' @param dfBounds `data.frame` Set of predicted percentages/rates and upper-
 #'   and lower-bounds across the full range of sample sizes/total exposure
 #'   values for reporting.
@@ -14,16 +12,11 @@
 #' @param dfResults `data.frame` A stacked summary of analysis pipeline output.
 #' @param dfSite `data.frame` Optional site-level metadata.
 #' @param dfStudy `data.frame` Optional study-level metadata.
-#' @param domain The type of data and subsequent metrics you would like to see.
 #' @param input Shiny inputs
 #' @param lAssessment `list` List of KRI assessments from `params` within
 #'   `KRIReport.Rmd`.
-#' @param metric The reactive value provided by the metric input from `server`
 #' @param output Shiny outputs
 #' @param session Shiny session
-#' @param site The reactive value provided by the site input from `server`
-#' @param subset_col The id column that will be selected from the domain.
-#' @param subset_val The specific value that the subset_col will be filtered by.
 #'
 #' @name shared-params
 #' @keywords internal
--- R/observe_site_select.R
+++ R/observe_site_select.R
@@ -1,40 +1,40 @@
-observe_site_select <- function(site) {
-  # Update site input when client-side selection occurs.
-  shiny::observeEvent(
-    site(),
-    {
-      cli::cli_alert_info(
-        "Selected site: {site()}"
-      )
-
-      shiny::updateSelectInput(
-        inputId = "site",
-        selected = site()
-      )
-
-      # Navigate to site details when a site is selected.
-      bslib::nav_select("primary_nav_bar", "Metric Details")
-
-      # Update list of participants when a site is selected.
-      # updateSelectizeInput(
-      #     inputId = 'participant',
-      #     selected = 'None',
-      #     choices = c(
-      #         'None',
-      #         snapshot$lInputs$lData$dfSUBJ %>%
-      #             dplyr::filter(
-      #                 site() == 'None' | .data[[ snapshot$lInputs$lMapping$dfSUBJ$strSiteCol ]] == site()
-      #             ) %>%
-      #             #dplyr::filter(
-      #             #    .data[[ snapshot$lInputs$lMapping$dfSUBJ$strEnrollCol ]] == snapshot$lInputs$lMapping$dfSUBJ$strEnrollVal
-      #             #) %>%
-      #             dplyr::pull(
-      #                 .data[[ snapshot$lInputs$lMapping$dfSUBJ$strIDCol ]]
-      #             ) %>%
-      #             sort()
-      #     )
-      # )
-    },
-    ignoreInit = TRUE
-  )
-}
+# observe_site_select <- function(site) {
+#   # Update site input when client-side selection occurs.
+#   shiny::observeEvent(
+#     site(),
+#     {
+#       cli::cli_alert_info(
+#         "Selected site: {site()}"
+#       )
+#
+#       shiny::updateSelectInput(
+#         inputId = "site",
+#         selected = site()
+#       )
+#
+#       # Navigate to site details when a site is selected.
+#       bslib::nav_select("primary_nav_bar", "Metric Details")
+#
+#       # Update list of participants when a site is selected.
+#       # updateSelectizeInput(
+#       #     inputId = 'participant',
+#       #     selected = 'None',
+#       #     choices = c(
+#       #         'None',
+#       #         snapshot$lInputs$lData$dfSUBJ %>%
+#       #             dplyr::filter(
+#       #                 site() == 'None' | .data[[ snapshot$lInputs$lMapping$dfSUBJ$strSiteCol ]] == site()
+#       #             ) %>%
+#       #             #dplyr::filter(
+#       #             #    .data[[ snapshot$lInputs$lMapping$dfSUBJ$strEnrollCol ]] == snapshot$lInputs$lMapping$dfSUBJ$strEnrollVal
+#       #             #) %>%
+#       #             dplyr::pull(
+#       #                 .data[[ snapshot$lInputs$lMapping$dfSUBJ$strIDCol ]]
+#       #             ) %>%
+#       #             sort()
+#       #     )
+#       # )
+#     },
+#     ignoreInit = TRUE
+#   )
+# }
