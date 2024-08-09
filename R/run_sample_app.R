71413186391b905943ec809ea315dab43756247d
Jon Harmon <jon.harmon1@gilead.com>
2024-08-09 13:57:43 -0500

    Add a function to run with sample data. (#166)
    Oops, I forgot we came up with a simple fix for removing the sample data from the main runner!
    
    Closes #153.

---
+++ R/run_sample_app.R
@@ -0,0 +1,20 @@
+#' Run the App with Sample Data
+#'
+#' Launch a version of the Shiny app built by this package, using sample data.
+#'
+#' @return An object that represents the app. Printing the object or passing it
+#'   to [shiny::runApp()] will run the app.
+#' @export
+#'
+#' @examplesIf interactive()
+#' run_sample_app()
+run_sample_app <- function() {
+  run_app(
+    dfResults = gsm::reportingResults %>%
+      dplyr::filter(.data$SnapshotDate == max(.data$SnapshotDate)),
+    dfGroups = gsm::reportingGroups,
+    dfMetrics = gsm::reportingMetrics,
+    dfBounds = gsm::reportingBounds %>%
+      dplyr::filter(.data$SnapshotDate == max(.data$SnapshotDate))
+  )
+}
