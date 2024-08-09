95b328fc21f92f4972f9d2dabc821af947d3baa3
Jon Harmon <jon.harmon@atorusresearch.com>
2024-08-09 13:15:04 -0500

    Add a function to run with sample data.
    Oops, I forgot we came up with a simple fix for removing the sample data from the main runner!
    
    Closes #153.

--- app.R
+++ app.R
@@ -3,11 +3,4 @@
 # Or use the blue button on top of this file
 pkgload::load_all(".")
 
-gsmApp::run_app(
-  dfResults = gsm::reportingResults %>%
-    dplyr::filter(.data$SnapshotDate == max(.data$SnapshotDate)),
-  dfGroups = gsm::reportingGroups,
-  dfMetrics = gsm::reportingMetrics,
-  dfBounds = gsm::reportingBounds %>%
-    dplyr::filter(.data$SnapshotDate == max(.data$SnapshotDate))
-)
+gsmApp::run_sample_app()
