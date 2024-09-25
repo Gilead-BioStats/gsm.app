bb1be709622c41d6966a2790c2d3e0604700e892
Jon Harmon <jon.harmon@atorusresearch.com>
2024-09-25 14:32:48 -0500

    Document gsmapp_abort.

--- R/utils-conditions.R
+++ R/utils-conditions.R
@@ -1,3 +1,13 @@
+#' Error messaging for this package.
+#'
+#' @inheritParams cli::cli_abort
+#' @param class A descriptive label for this type of error, in lower_snake_case.
+#' @param .envir Environment in which any variables used in the message are
+#'   defined. You almost definitely want to leave this as the default value.
+#'
+#' @return An error condition with classes `"gsm.app-condition"`,
+#'   `"gsm.app-error"`, and `"gsm.app-error-{class}"`.
+#' @keywords internal
 gsmapp_abort <- function(message, class, ..., .envir = rlang::caller_env()) {
   cli::cli_abort(
     message,
