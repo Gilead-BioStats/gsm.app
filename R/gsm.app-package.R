4148518018152d3eb8d0277da6c964156b5651d2
Jon Harmon <jon.harmon@atorusresearch.com>
2024-10-14 15:19:35 -0500

    Add gt to Imports to elevate from gsm Suggests.

--- R/gsm.app-package.R
+++ R/gsm.app-package.R
@@ -5,6 +5,7 @@
 #' @import shiny
 #' @importFrom cli cli_alert
 #' @importFrom cli cli_alert_info
+#' @importFrom gt gt
 #' @importFrom kableExtra kbl
 #' @importFrom rlang .data
 ## usethis namespace: end
