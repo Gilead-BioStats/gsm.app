19fb0fef16b04cdd8cbf92e9950c77acc2ff9c11
Jon Harmon <jon.harmon1@gilead.com>
2024-10-28 14:30:27 -0500

    Use Report_StudyInfo() in sidebar. (#291)
    * Use Report_StudyInfo() in sidebar.
    
    See https://github.com/Gilead-BioStats/gsm/issues/1905 for a ticket that would make this quite a bit simpler.
    
    Closes #288.
    
    * Update with latest gsm.
    
    I kept the old KRI flag badge colors as "light" iterations for upcoming dev, even though they aren't currently used.
    
    * Update gsm.

--- tests/testthat/test-out_StudyInformation.R
+++ tests/testthat/test-out_StudyInformation.R
@@ -1,6 +1,5 @@
 test_that("out_StudyInformation shows study info", {
-  lStudy <- make_lStudy(sample_dfGroups, sample_dfResults)
-  test_result <- out_StudyInformation(lStudy)
+  test_result <- out_StudyInformation(sample_dfGroups, sample_dfResults)
   expect_s3_class(test_result, c("bslib_fragment", "shiny.tag"))
   class(test_result) <- "shiny.tag"
   expect_cleaned_html(test_result)
