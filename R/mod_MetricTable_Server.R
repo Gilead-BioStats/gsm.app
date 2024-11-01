0b3beee5caea63863457c7290e339a42e12b2443
Jon Harmon <jon.harmon@atorusresearch.com>
2024-11-01 13:09:36 -0500

    Use updated {gt} for Analysis Output.
    Closes #294.

--- R/mod_MetricTable_Server.R
+++ R/mod_MetricTable_Server.R
@@ -10,63 +10,113 @@ mod_MetricTable_Server <- function(
   rctv_strSiteID
 ) {
   moduleServer(id, function(input, output, session) {
-    output$table <- gt::render_gt({
-      req(rctv_dfResults())
-      tbl <- gsm::Report_MetricTable(
+    # Update the widget when the source data changes.
+    rctv_tbl <- shiny::reactive({
+      gsm::Report_MetricTable(
         rctv_dfResults(),
         dfGroups = dfGroups,
         strGroupLevel = "Site"
       )
-      # Hack to fix `Enrolled` sorting. See
-      # https://github.com/Gilead-BioStats/gsm/issues/1895
-      tbl$`_data`$Enrolled <- as.integer(tbl$`_data`$Enrolled)
+    })
 
+    # Extract the data back out of the widget.
+    rctv_tblData <- shiny::reactive({
+      shiny::req(rctv_tbl())
+      tbl <- rctv_tbl()
+      if (inherits(tbl, "gt_tbl")) {
+        return(tbl$`_data`)
+      }
+      # Return an empty version of the tibble
+      tibble::tibble(
+        StudyID = character(),
+        GroupID = character(),
+        MetricID = character(),
+        Group = character(),
+        SnapshotDate = head(as.Date(1), 0),
+        Enrolled = integer(),
+        Numerator = numeric(),
+        Denominator = numeric(),
+        Metric = numeric(),
+        Score = numeric(),
+        Flag = numeric()
+      )
+    })
+
+    # The RV ensures that the selection gets updated when the table
+    # re-initializes. I'm trying to figure out if this is a general issue and
+    # whether it can be dealt with more elegantly in gt.
+    rctv_widget_update <- reactiveVal()
+
+    # Render the table interactively.
+    output$table <- gt::render_gt({
+      req(rctv_dfResults())
+      tbl <- rctv_tbl()
+      rctv_widget_update("updating")
       tbl %>%
         gt::opt_interactive(
           use_resizers = TRUE,
           use_highlight = TRUE,
           use_compact_mode = TRUE,
           use_text_wrapping = FALSE,
-          use_page_size_select = TRUE
+          use_page_size_select = TRUE,
+          selection_mode = "single"
         ) %>%
         gt::tab_options(
           table.background.color = "transparent",
           column_labels.background.color = "transparent"
         ) %>%
-        gt::opt_row_striping()
-    })
+        gt::opt_row_striping() %>%
+        gt::opt_css(
+          "/* Hide the selection column */
+           .rt-td-select {
+             display: none;
+           }
 
-    observe({
-      session$sendCustomMessage(
-        "gtSetSelectID",
-        list(
-          id = session$ns("table"),
-          selectID = rctv_strSiteID()
+           /* Style for selected rows */
+           .rt-tr-selected {
+             background-color: black !important;
+             color: white;
+           }"
         )
-      )
     })
 
     # Reactive value to store the selected row
     selected_row <- reactiveVal("None")
 
-    # Whenever the table changes, re-bind click events.
-    observeEvent(rctv_dfResults(), {
-      session$sendCustomMessage("gtBindClick", list(id = session$ns("table")))
-    })
-
     # Observe table selections
     observe({
-      req(rctv_dfResults())
-      if (length(input$table) > 0) {
-        selected_row(input$table)
+      req(rctv_tblData())
+      tbl_data <- rctv_tblData()
+      if (is.null(input$table)) {
+        selected_row(NULL)
       } else {
-        selected_row("None")
+        if (nrow(tbl_data) && !all(input$table == 0)) {
+          if (!identical(input$table, selected_row())) {
+            selected_row(tbl_data$GroupID[[input$table]])
+          }
+        } else {
+          selected_row("None")
+        }
       }
+    }) %>%
+      shiny::bindEvent(input$table, ignoreInit = TRUE, ignoreNULL = FALSE)
+
+    # Update selections from outside.
+    observe({
+      req(rctv_strSiteID())
+      req(rctv_tblData())
+      # We need to "use" this to make sure we always update the selection after
+      # the table loads. I'm trying to figure out whether this is something that
+      # *can* be fixed at the gt level.
+      force(rctv_widget_update())
+      gt::gt_update_select(
+        "table",
+        which(rctv_tblData()$GroupID %in% rctv_strSiteID())
+      )
+      rctv_widget_update(NULL)
     })
 
     # Return the selected row data
-    return(reactive({
-      selected_row()
-    }))
+    return(selected_row)
   })
 }
