e9dd9e7d3fb214f55242455650be56dcd328099a
Jon Harmon <jon.harmon@atorusresearch.com>
2024-11-04 11:54:37 -0600

    Remove kableExtra req.

--- R/gsm.app-package.R
+++ R/gsm.app-package.R
@@ -6,11 +6,7 @@
 #' @importFrom cli cli_alert
 #' @importFrom cli cli_alert_info
 #' @importFrom gt gt
-#' @importFrom kableExtra kbl
 #' @importFrom rlang %||%
 #' @importFrom rlang .data
 ## usethis namespace: end
 NULL
-
-# kableExtra::kbl is imported for Suggests use in {gsm}, no longer needed once
-# that use case is removed.
