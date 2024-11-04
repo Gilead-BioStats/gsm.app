f3081da90fee0b059686ee70fa45cc1c33b73cd2
Jon Harmon <jon.harmon@atorusresearch.com>
2024-11-04 09:45:19 -0600

    Allow favicon to switch easily, and switch it for PR/dev.

--- app.R
+++ app.R
@@ -3,4 +3,7 @@
 # Or use the blue button on top of this file
 pkgload::load_all(".", helpers = FALSE, attach_testthat = FALSE)
 
-run_sample_gsm_app()
+run_sample_gsm_app(
+  strFavicon = Sys.getenv("GSMAPP_FAVICON", "angles-up"),
+  strFaviconColor = Sys.getenv("GSMAPP_FAVICONCOLOR", colorScheme("red"))
+)
