a56bb7188f24b849c167ca2b5bf14f863d75f68c
Jon Harmon <jon.harmon@atorusresearch.com>
2025-04-25 15:06:41 -0500

    Clean up plugin definition functions. Closes #440.

--- R/plugin_Define.R
+++ R/plugin_Define.R
@@ -1,24 +1,13 @@
 #' Define a plugin
 #'
 #' @inheritParams shared-params
-#' @inheritParams rlang::args_dots_empty
 #' @param strName `length-1 character` The name of the plugin, as it will
 #'   display to the user in the menu of tabs and plugins.
-#' @param lSpec `list` A named list defining the data domains required by the
-#'   plugin, where the names are the names of the domains and the elements are
-#'   column definitions.
-#' @param fnShinyUI `function or character` A shiny module UI function for the
-#'   plugin, or the name of such a function in the current session.
-#' @param fnShinyServer `function or character` A shiny module server function
-#'   for the plugin, or the name of such a function in the current session.
-#' @param chrRequiredInputs `character` An optional vector of any inputs
-#'   ("Metric", "Site", "Participant", or "Domain") that must have a non-empty
-#'   value before the plugin can load. "None" and "All" count as "empty" for
-#'   this check. If the user has not set a value for that input, the app will
-#'   display a placeholder instructing the user to make a selection.
+#' @param ... Additional named arguments passed on to `fnShinyUI` and/or
+#'   `fnShinyServer` by name.
 #'
-#' @returns A `list` with elements `meta`, `shiny`, `domains`, and (optionally)
-#'   `required_inputs`.
+#' @returns A `list` with elements `meta`, `shiny`, `spec`, `workflows`,
+#'   `config`, `required_inputs`, and `packages`.
 #' @export
 #'
 #' @examples
@@ -35,21 +24,12 @@ plugin_Define <- function(
   fnShinyUI,
   fnShinyServer,
   ...,
+  lWorkflows = list(),
   chrRequiredInputs = character(),
   envCall = rlang::caller_env()
 ) {
-  rlang::check_dots_empty()
-  CheckIsString(strName)
-  if (length(chrRequiredInputs)) {
-    CheckIsIn(
-      tolower(chrRequiredInputs),
-      c("metric", "site", "participant", "domain"),
-      "chrRequiredInputs",
-      envCall
-    )
-  }
-
-  lPluginDefinition <- list(
+  dots <- rlang::list2(...)
+  lPlugin <- list(
     meta = list(Name = strName),
     shiny = list(
       UI = fnShinyUI,
@@ -58,5 +38,151 @@ plugin_Define <- function(
     spec = lSpec,
     required_inputs = chrRequiredInputs
   )
-  return(purrr::keep(lPluginDefinition, rlang::has_length))
+  return(
+    plugin_Prepare(lPlugin, lWorkflows, ..., envCall = envCall)
+  )
+}
+
+#' Finish Plugin Definition
+#'
+#' @inheritParams shared-params
+#' @param ... Additional named arguments passed on to `fnShinyUI` and/or
+#'   `fnShinyServer` by name.
+#' @returns The validated `lPlugin`.
+#' @keywords internal
+plugin_Prepare <- function(
+  lPlugin,
+  lWorkflows = list(),
+  ...,
+  envCall = rlang::caller_env()
+) {
+  lPlugin$workflows <- lWorkflows
+  lPlugin$config <- rlang::list2(...)
+  return(plugin_ValidateDefinition(lPlugin, envCall))
+}
+
+#' Validate Plugin Definition
+#'
+#' @inheritParams shared-params
+#' @returns The validated `lPlugin`.
+#' @keywords internal
+plugin_ValidateDefinition <- function(lPlugin, envCall = rlang::caller_env()) {
+  lPlugin <- purrr::keep(lPlugin, rlang::has_length)
+  chrRequiredFields <- c("meta", "shiny")
+  chrOptionalFields <- c(
+    "spec", "packages", "required_inputs", "workflows", "config"
+  )
+  CheckHasAllFields(
+    lPlugin,
+    chrRequiredFields,
+    "Plugin definitions",
+    envCall
+  )
+  CheckHasOnlyFields(
+    lPlugin,
+    c(chrRequiredFields, chrOptionalFields),
+    "Plugin definitions",
+    envCall
+  )
+  lPlugin$meta <- plugin_ValidateMeta(lPlugin$meta, envCall)
+  lPlugin$shiny <- plugin_ValidateShiny(lPlugin$shiny, envCall)
+  # TODO: Validate spec
+  lPlugin$spec <- lPlugin$spec %||% list()
+  lPlugin$packages <- plugin_ValidatePackages(lPlugin$packages, envCall)
+  lPlugin$required_inputs <- plugin_ValidateReqs(lPlugin$required_inputs, envCall)
+  # TODO: Validate workflows
+  lPlugin$workflows <- lPlugin$workflows %||% list()
+  lPlugin$config <- lPlugin$config %||% list()
+
+  return(lPlugin)
+}
+
+#' Validate Plugin Definition Meta
+#'
+#' @inheritParams shared-params
+#' @param lPluginMeta `list` The plugin metadata.
+#' @returns The validated `lPlugin$meta`.
+#' @keywords internal
+plugin_ValidateMeta <- function(lPluginMeta, envCall = rlang::caller_env()) {
+  CheckHasAllFields(
+    lPluginMeta,
+    "Name",
+    "Plugin definitions",
+    envCall
+  )
+  CheckIsString(lPluginMeta$Name)
+  return(lPluginMeta)
+}
+
+#' Validate Plugin Definition Shiny
+#'
+#' @inheritParams shared-params
+#' @param lPluginShiny `list` The UI and Server function of the plugin.
+#' @returns The validated `lPlugin$shiny`.
+#' @keywords internal
+plugin_ValidateShiny <- function(lPluginShiny, envCall = rlang::caller_env()) {
+  CheckHasAllFields(
+    lPluginShiny,
+    c("UI", "Server"),
+    "Plugin definition shiny sections",
+    envCall
+  )
+  CheckHasOnlyFields(
+    lPluginShiny,
+    c("UI", "Server"),
+    "Plugin definition shiny sections",
+    envCall
+  )
+  return(lPluginShiny)
+}
+
+#' Validate Plugin Definition Packages
+#'
+#' @inheritParams shared-params
+#' @param lPluginPackages `list` The names and sources of packages used by the
+#'   plugin.
+#' @returns The validated `lPlugin$packages`.
+#' @keywords internal
+plugin_ValidatePackages <- function(
+  lPluginPackages,
+  envCall = rlang::caller_env()
+) {
+  if (length(lPluginPackages)) {
+    for (pkg in lPluginPackages) {
+      CheckHasAllFields(
+        pkg,
+        "name",
+        "Plugin definition package requirements",
+        envCall
+      )
+      CheckHasOnlyFields(
+        pkg,
+        c("name", "remote"),
+        "Plugin definition package requirements",
+        envCall
+      )
+    }
+  }
+  return(lPluginPackages %||% list())
+}
+
+#' Validate Plugin Definition Required Inputs
+#'
+#' @inheritParams shared-params
+#' @returns The validated `lPlugin$required_inputs`.
+#' @keywords internal
+plugin_ValidateReqs <- function(
+  chrRequiredInputs,
+  envCall = rlang::caller_env()
+) {
+  if (length(chrRequiredInputs)) {
+    chrRequiredInputs <- tolower(chrRequiredInputs)
+    CheckIsIn(
+      chrRequiredInputs,
+      c("metric", "site", "participant", "domain"),
+      "Requird inputs",
+      envCall
+    )
+  }
+  return(chrRequiredInputs %||% character())
 }
