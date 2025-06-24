11210ca737db380ee144127c06c780c95a3245ed
Jon Harmon <jon.harmon@atorusresearch.com>
2025-06-24 16:10:58 -0500

    Properly pass strGroupLevel to gsmViz.default.groupOverview JS.
    Closes #492.

--- tests/testthat/test-run_gsm_app.R
+++ tests/testthat/test-run_gsm_app.R
@@ -101,6 +101,40 @@ test_that("run_gsm_app populates Study Overview", {
   app$stop()
 })
 
+test_that("run_gsm_app Study Overview works with GroupLevel", {
+  skip_on_cran()
+  app <- AppDriver$new(
+    test_path("apps", "standard"),
+    name = "study",
+    width = 1200,
+    height = 1200
+  )
+  app$wait_for_idle()
+  app$wait_for_value(input = "participant", timeout = 2000)
+
+  # When Group Level changes, Enrolled column is populated.
+  app$set_inputs(`group-level` = "Country")
+  app$run_js(file = test_path("fixtures", "WidgetPlotTestHelpers.js"))
+  app$wait_for_js(
+    "isCanvasLoaded('metric_details-scatter_plot');",
+    timeout = 2000
+  )
+  app$wait_for_idle()
+  app$set_inputs(primary_nav_bar = "Study Overview")
+  app$wait_for_idle()
+  app$set_inputs(`study_overview-table-kri_counts-red-toggle_pill` = "click")
+  app$wait_for_idle()
+  app$expect_values(
+    export = TRUE,
+    name = "enrolled",
+    screenshot_args = list(
+      selector = list(selector = "table.group-overview")
+    )
+  )
+
+  app$stop()
+})
+
 test_that("run_gsm_app populates Metric Details", {
   skip_on_cran()
   app <- AppDriver$new(
@@ -341,7 +375,7 @@ test_that("run_gsm_app populates Domain Details", {
   app$stop()
 })
 
-test_that("run_gsm_app display Domain error", {
+test_that("run_gsm_app displays Domain error", {
   skip_on_cran()
   app <- AppDriver$new(
     test_path("apps", "error"),
@@ -368,7 +402,7 @@ test_that("run_gsm_app display Domain error", {
   app$stop()
 })
 
-test_that("run_gsm_app display participant plugin", {
+test_that("run_gsm_app displays participant plugin", {
   skip_on_cran()
   app <- AppDriver$new(
     test_path("apps", "plugin"),
